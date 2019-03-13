85a448fdc670c92abf2b56350ad5c2210a3a3f7b
Reinald Reynoso <reireynoso@gmail.com>
2019-03-13 16:52:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,14 @@
 
 def key_for_min_value(name_hash)
   lowest_num = 0
-  min_key = ""
+  min_key = 
   name_hash.each do |key, value|
-    
-    if lowest_num == 0 
-      lowest_num = value
-      min_key = key
+     if lowest_num == 0 
+        lowest_num = value
+        min_key = key
     elsif value < lowest_num
-      lowest_num = value
-      min_key = key
+        lowest_num = value
+        min_key = key
     end
   end
   return min_key
