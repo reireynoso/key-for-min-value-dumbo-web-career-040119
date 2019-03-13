03163a036f98e0e36e162659c50f4614dc9e54b3
Reinald Reynoso <reireynoso@gmail.com>
2019-03-13 16:48:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_num = 0
   min_key = ""
   name_hash.each do |key, value|
-    if key == nil || value == nil
+    if key == nil && value == nil
       min_key = nil
     elsif lowest_num == 0 
       lowest_num = value
